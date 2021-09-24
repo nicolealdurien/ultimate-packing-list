d88fc802609a00ba31f0ddfd59a2e65f20035313
naldurien <nicole.aldurien@gmail.com>
2021-09-24 00:13:35 -0400

    basic working app, styling needed

--- server/app.js
+++ server/app.js
@@ -10,18 +10,98 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 app.use(express.urlencoded())
 
-// db.any('SELECT item_id, name, subcategory, is_on_list, quantity FROM items')
-//     .then((items) => {
-//         console.log(items)
-//     })
+app.post('/add-item', (req, res) => {
+    const { name, subcategory } = req.body
+    const quantity = parseInt(req.body.quantity)
+    db.none('INSERT INTO items(name, subcategory, quantity) VALUES($1, $2, $3)', [name, subcategory, quantity])
+    .then(() => {
+        res.redirect('/')
+    })
+})
+
+app.post('/update-item', (req,res) => {
+    let itemId = parseInt(req.body.item_id)
+    let isPacked = req.body.isPacked == "on" ? true : false
+    
+    db.none('UPDATE items SET is_packed = $1 WHERE item_id = $2', [isPacked, itemId])
+    .then(() => {
+        res.redirect('/')
+    })
+})
+
+app.post('/pack-item', (req,res) => {
+    let itemId = parseInt(req.body.item_id)
+    let isPacked = true
+    
+    db.none('UPDATE items SET is_packed = $1 WHERE item_id = $2', [isPacked, itemId])
+    .then(() => {
+        res.redirect('/')
+    })
+})
+
+app.post('/update-list', (req,res) => {
+    let itemId = parseInt(req.body.item_id)
+    let isOnList = true
+    
+    db.none('UPDATE items SET is_on_list = $1 WHERE item_id = $2', [isOnList, itemId])
+    .then(() => {
+        res.redirect('/master-list')
+    })
+})
+
+app.post('/unpack', (req,res) => {
+    let itemId = parseInt(req.body.item_id)
+    let isPacked = false
+    
+    db.none('UPDATE items SET is_packed = $1 WHERE item_id = $2', [isPacked, itemId])
+    .then(() => {
+        res.redirect('/already-packed')
+    })
+})
+
+app.post('/de-list', (req,res) => {
+    let itemId = parseInt(req.body.item_id)
+    let isOnList = false
+    let isPacked = false
+    
+    db.none('UPDATE items SET is_on_list = $1, is_packed = $2 WHERE item_id = $3', [isOnList, isPacked, itemId])
+    .then(() => {
+        res.redirect('/already-packed')
+    })
+})
+
+app.post('/delete-item', (req, res) => {
+    const { itemId } = req.body
+    db.none('DELETE FROM items WHERE item_id = $1;', [bookId])
+    .then(() => {
+        res.redirect('/')
+    })
+})
 
 app.get('/', (req, res) => {
-    db.any('SELECT item_id, name, subcategory, is_on_list, quantity FROM items')
+    db.any('SELECT item_id, name, subcategory, is_on_list, is_packed, quantity FROM items WHERE is_on_list = true AND is_packed = false')
     .then((items) => {
         res.render('index', { items: items })
     })
 })
 
+app.get('/master-list', (req, res) => {
+    db.any('SELECT item_id, name, subcategory, is_on_list, quantity FROM items where is_on_list = false')
+    .then((items) => {
+        res.render('master-list', { items: items })
+    })
+})
+
+app.get('/already-packed', (req, res) => {
+    db.any('SELECT item_id, name, subcategory, is_on_list, quantity FROM items where is_packed = true')
+    .then((items) => {
+        res.render('already-packed', { items: items })
+    })
+})
+
+app.get('/add-item', (req, res) => {
+    res.render('add-item')
+  })
 
 
 
