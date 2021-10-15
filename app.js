9020ac7ce2a0180b878ebd70697cca485d8e4944
nicolealdurien <nicole@lumenlearning.com>
2021-10-15 15:25:12 -0400

    testing git from new laptop

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.get('/register', (req, res) => {
     res.render('register')
 })
 
-// Register new user with hashed password
+// Register new user and store their password as a hash
 app.post('/register', (req, res) => {
     const username = req.body.username
     const password = req.body.password
